d41ef2d72a0d8fdc993c1e9cb07d9d2c878a994d
amit reuveni <amitr123r@gmail.com>
2025-08-05 20:51:49 +0300

    skeleton html css ts

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,25 @@
 var Player = /** @class */ (function () {
-    function Player() {
+    function Player(startX, startY) {
+        this.location = { x: startX, y: startY };
+        this.width = 30;
+        this.height = 30;
+        this.speed = 5;
     }
+    Player.prototype.render = function (container) {
+        this.playerElement = document.createElement("div");
+        this.playerElement.className = "game-player";
+        container.appendChild(this.playerElement);
+    };
+    Player.prototype.move = function (moveX, moveY) {
+        this.location.x += moveX;
+        this.location.y += moveY;
+        this.updatePosition();
+    };
+    Player.prototype.updatePosition = function () {
+        this.playerElement.style.left = this.location.x + "px";
+        this.playerElement.style.top = this.location.y + "px";
+    };
     return Player;
 }());
+var player = new Player(0, 0);
+console.log(player);
--- script.ts
+++ script.ts
@@ -1,3 +1,38 @@
 class Player {
-    position: {x: number; y: number;}   
+    location: {x: number; y:number}
+    width: number;
+    height: number;
+    speed: number;
+    playerElement!: HTMLElement;
+
+    constructor(startX: number, startY: number) {
+        this.location = {x: startX, y:startY};
+        this.width = 30;
+        this.height = 30;
+        this.speed = 5;
+
+
+    }
+
+    render(container: HTMLElement) {
+        this.playerElement = document.createElement("div");
+        this.playerElement.className = "game-player";
+
+        container.appendChild(this.playerElement);
+    }
+
+   move(moveX: number, moveY: number) {
+        this.location.x += moveX;
+        this.location.y += moveY;
+
+        this.updatePosition();
+    }
+
+    updatePosition() {
+        this.playerElement.style.left = this.location.x + "px";
+        this.playerElement.style.top = this.location.y + "px";
+    }
 }
+
+const player = new Player(0, 0);
+console.log(player);
