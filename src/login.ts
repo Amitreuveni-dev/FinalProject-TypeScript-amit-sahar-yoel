df82ddf4e36be7b87f9b784c2fc4e9f6c678a8f0
sahar <saharshabat@gmail.com>
2025-08-11 17:46:21 +0300

    login

--- src/login.ts
+++ src/login.ts
@@ -5,41 +5,47 @@ class Login {
   private correctUsername = "admin";
   private correctPassword = "1234";
 
-  constructor(username:string,password:any){
+  constructor(username: string, password: any) {
     this.username = username;
     this.password = password;
   }
 
-  checkCredentials():boolean{
-    return this.username === this.correctUsername && this.password === this.correctPassword;
+  checkCredentials(): boolean {
+    return (
+      this.username === this.correctUsername &&
+      this.password === this.correctPassword
+    );
   }
 }
 
-document.addEventListener("DOMContentLoaded",()=>{
-const form = document.getElementById("loginDetails") as HTMLFormElement;
-form.addEventListener("submit",(event)=>{
-  event.preventDefault();
-  const username = document.getElementById("username") as HTMLInputElement;
-  const password = document.getElementById("password") as HTMLInputElement;
-  const loginInput = new Login(username.value,password.value);
-  if(loginInput.checkCredentials()){
-    // alert("loged in")
-    window.location.href="index.html"
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginDetails") as HTMLFormElement;
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const username = document.getElementById("username") as HTMLInputElement;
+    const password = document.getElementById("password") as HTMLInputElement;
+    const loginInput = new Login(username.value, password.value);
+    if (loginInput.checkCredentials()) {
+      // alert("loged in")
+      window.location.href = "index.html";
+      form.reset();
+      return;
+    }
+    alert("try again");
     form.reset();
-    return;
-  }
-  alert("try again")
-  form.reset();
-})
-})
+  });
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   const usernameInput = document.getElementById("username") as HTMLInputElement;
   const passwordInput = document.getElementById("password") as HTMLInputElement;
-  const loginButton = document.querySelector("button[type='submit']") as HTMLButtonElement;
+  const loginButton = document.querySelector(
+    "button[type='submit']"
+  ) as HTMLButtonElement;
 
   function toggleButtonState() {
-    const isFormValid = usernameInput.value.trim() !== "" && passwordInput.value.trim() !== "";
+    const isFormValid =
+      usernameInput.value.trim() !== "" && passwordInput.value.trim() !== "";
     loginButton.disabled = !isFormValid;
   }
 
